9af06e5c6cbdb714a0aad5182059aae5484b1492
Vinz <vinz.pimentel@gmail.com>
2026-04-13 10:49:01 +0800

    Added message() in anotherone.java

--- anotherone.java
+++ anotherone.java
@@ -1,10 +1,12 @@
 public class anotherone {
     private boolean isValid;
     private int statuscode;
+    private String message;
 
     public anotherone(boolean isValid, int statuscode) {
         this.isValid = isValid;
         this.statuscode = statuscode;
+        message = "Status code is " + statuscode + " and validity is " + isValid;
     }
 
     public boolean getValid() {
@@ -12,6 +14,10 @@ public class anotherone {
     }
     public int getStatuscode() {
         return statuscode;
+    }
+
+    public String getMessage() {
+        return message;
     }
     
 }
