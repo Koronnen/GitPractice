04ffc14fdd162a3cb34341712b1930511bba8884
Vinz <vinz.pimentel@gmail.com>
2026-04-13 09:41:52 +0800

    Initial Commit

---
+++ anotherone.java
@@ -0,0 +1,10 @@
+public class anotherone {
+    private boolean isValid;
+    private int statuscode;
+
+    public anotherone(boolean isValid, int statuscode) {
+        this.isValid = isValid;
+        this.statuscode = statuscode;
+    }
+    
+}
